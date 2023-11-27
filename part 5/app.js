da6410de8855a7c2850d7df00fb7e891db2fdc88
skmirajbn <skmirajbn@gmail.com>
2023-11-27 22:41:12 +0600

    updated

---
+++ part 5/app.js
@@ -0,0 +1,17 @@
+let app = Vue.createApp({
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+});
+
+app.mount("#app");
