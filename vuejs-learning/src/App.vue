377756937382b1a71d7e5510c27fa3ed3b5e6d74
skmirajbn <skmirajbn@gmail.com>
2023-11-29 01:34:37 +0600

    updated

---
+++ vuejs-learning/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <contact-details></contact-details>
+  </div>
+</template>
+
+<script>
+import ContactDetails from "./ContactDetails.vue";
+export default {
+  data() {
+    return {
+      message: "Welcome to vue3 Learning",
+    };
+  },
+  methods: {},
+  components: {
+    ContactDetails,
+  },
+};
+</script>
---
+++ vuejs-learning/src/ContactDetails.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>Name: {{ name }}</h3>
+    <h3>Mobile: {{ mobile }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Sk Miraj",
+      mobile: "01700 000 000",
+    };
+  },
+};
+</script>
