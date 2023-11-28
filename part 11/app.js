ec4cfc7c2e15381d50f7912fa6158cf5ac48cf17
skmirajbn <skmirajbn@gmail.com>
2023-11-28 20:47:19 +0600

    updated

--- part 11/app.js
+++ part 11/app.js
@@ -1,14 +1,22 @@
 let app = Vue.createApp({
   data() {
     return {
-      skills: ["HTML", "CSS", "JS", "VueJs"],
+      skills: [
+        { name: "HTML", experience: 10 },
+        { name: "CSS", experience: 5 },
+        { name: "JS", experience: 10 },
+        { name: "VueJs", experience: 10 },
+      ],
       newSkill: "",
     };
   },
   methods: {
     addSkill() {
-      this.skills.push(this.newSkill);
+      this.skills.push({ name: this.newSkill, experience: 0 });
       this.newSkill = "";
+    },
+    removeSkill(index) {
+      this.skills.splice(index, 1);
     },
   },
 });
