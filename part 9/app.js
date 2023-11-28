2a5ac8a6140c4a7df12ec20f4033df07042cc0e8
skmirajbn <skmirajbn@gmail.com>
2023-11-28 19:54:27 +0600

    updated

--- part 9/app.js
+++ part 9/app.js
@@ -1,27 +1,12 @@
 let app = Vue.createApp({
   data() {
     return {
-      num: "",
-      result: "",
+      activeClass: "red",
     };
   },
   methods: {
-    handleInput(e) {
-      console.log(e);
-      this.num = e.target.value;
-    },
-    getDouble() {
-      this.result = this.num * 2;
-    },
-    getSquare() {
-      this.result = this.num * this.num;
-    },
-    addTen() {
-      this.result = this.num + 10;
-    },
-    reset() {
-      this.num = "";
-      this.result = "";
+    setClass(className) {
+      this.activeClass = className;
     },
   },
 });
