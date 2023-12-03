21b6bebaa5b4ed04a1b2a7603c07da43a2933143
skmirajbn <skmirajbn@gmail.com>
2023-12-03 20:32:27 +0600

    updated

--- vuejs-vite/src/App.vue
+++ vuejs-vite/src/App.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>Hello</div>
+  <header>
+    <h2>{{ msg }}</h2>
+  </header>
+
+  <div>
+    <input type="text" placeholder="Enter your name" ref="nameInput" />
+    <the-comment heading="This is the heading from props"></the-comment>
+  </div>
 </template>
 
 <script>
-export default {};
+import TheComment from "./TheComment.vue";
+
+export default {
+  data() {
+    return {
+      msg: "Hello World",
+    };
+  },
+  mounted() {
+    console.log(this.$refs.nameInput);
+  },
+  components: {
+    TheComment,
+  },
+};
 </script>
 
 <style scoped></style>
---
+++ vuejs-vite/src/TheComment.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>{{ heading }}</h3>
+    <div>
+      <input type="text" />
+      <button>Submit</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  heading: String,
+});
+</script>
+
+<style scoped></style>
