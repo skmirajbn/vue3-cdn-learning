cf8a43f1d0ac20a8260fee9f632095e805d093f1
skmirajbn <skmirajbn@gmail.com>
2023-12-03 21:45:37 +0600

    updated

--- vuejs-vite/src/TheComment.vue
+++ vuejs-vite/src/TheComment.vue
@@ -1,21 +1,45 @@
 <template>
   <div>
-    <h3>{{ heading }}</h3>
+    <h3>{{ msg }}</h3>
+
     <div>
-      <input type="text" v-model="inputValue" />
-      <button>Submit</button>
+      <h1>New User Registration</h1>
+      <label for="fullName">Full Name</label>
+      <input type="text" id="fullName" v-model="formData.fullName" />
+
+      <br /><br />
+
+      <label for="fullName">Abour You</label>
+      <textarea v-model="formData.aboutYou" placeholder="Tell us about yourself"></textarea>
     </div>
+
+    <br /><br />
+    <label>Gender</label><br />
+    <label><input type="radio" name="gender" value="male" v-model="formData.gender" /> Male</label>
+    <label><input type="radio" name="gender" value="female" v-model="formData.gender" /> Female</label>
+    <label><input type="radio" name="gender" value="other" v-model="formData.gender" /> Other</label>
+  </div>
+  <hr />
+
+  <div v-for="(value, key) in formData" :key="key">
+    <strong>{{ key }} : </strong> {{ value }}
   </div>
 </template>
 
-<script setup>
-import { defineProps, ref } from "vue";
-
-const props = defineProps({
-  heading: String,
-});
-
-const inputValue = ref("");
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello World",
+      name: "",
+      formData: {
+        fullName: "",
+        aboutYou: "",
+        gender: "",
+      },
+    };
+  },
+};
 </script>
 
 <style scoped></style>
